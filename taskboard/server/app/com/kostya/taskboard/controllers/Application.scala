d77bb7ba3f8c505b0bd81d59c82c466edb3fbbc1
KonstmarNSK <konstmar@yandex.ru>
2020-02-27 11:55:57 +0700

    initial commit

---
+++ taskboard/server/app/com/kostya/taskboard/controllers/Application.scala
@@ -0,0 +1,15 @@
+package com.kostya.taskboard.controllers
+
+import javax.inject._
+
+import com.kostya.taskboard.shared.SharedMessages
+import play.api.mvc._
+
+@Singleton
+class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def index = Action {
+    Ok(views.html.index(SharedMessages.itWorks))
+  }
+
+}
---
+++ taskboard/shared/src/main/scala/com/kostya/taskboard/shared/SharedMessages.scala
@@ -0,0 +1,5 @@
+package com.kostya.taskboard.shared
+
+object SharedMessages {
+  def itWorks = "It works!"
+}
